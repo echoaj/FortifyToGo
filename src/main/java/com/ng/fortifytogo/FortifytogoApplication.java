4589981d34a6bc377ddd5148e9fa3726e243430e
echoaj <aljoslin13@yahoo.com>
2023-08-04 13:15:04 -0700

    first commit

---
+++ src/main/java/com/ng/fortifytogo/FortifytogoApplication.java
@@ -0,0 +1,14 @@
+package com.ng.fortifytogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortifytogoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(FortifytogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ng/fortifytogo/FortifytogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ng.fortifytogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortifytogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
