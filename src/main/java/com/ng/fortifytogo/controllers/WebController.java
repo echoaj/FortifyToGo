9f90600b4c0a72bd67a5fd604d4b9c178189340c
echoaj <aljoslin13@yahoo.com>
2023-08-12 14:49:47 -0700

    completed git download function

--- src/main/java/com/ng/fortifytogo/controllers/WebController.java
+++ src/main/java/com/ng/fortifytogo/controllers/WebController.java
@@ -1,5 +1,7 @@
 package com.ng.fortifytogo.controllers;
 
+import com.ng.fortifytogo.services.GitService;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,14 +11,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WebController {
 
+    private final GitService gitService = new GitService();
+
     @GetMapping(path="/" )
     public String home(){
         return "home";
     }
 
     @PostMapping(path="/generateReport")
-    public String genReport(@RequestParam("githubLink") String githubLink, Model model){
+    public String genReport(@RequestParam("githubLink") String githubLink, Model model) throws GitAPIException {
         System.out.println(githubLink);
+        String localRepoPath = gitService.cloneRepository(githubLink, "C:\\Users\\aljos\\OneDrive\\Desktop\\AlexShare");
+        System.out.println(localRepoPath);
         model.addAttribute("githubLink", githubLink);
         return "report";
     }
--- src/main/java/com/ng/fortifytogo/services/GitService.java
+++ src/main/java/com/ng/fortifytogo/services/GitService.java
@@ -1,8 +1,21 @@
 package com.ng.fortifytogo.services;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 public class GitService {
+
+    public String cloneRepository(String repoUrl, String destinationDir) throws GitAPIException {
+        try (Git git = Git.cloneRepository()
+                .setURI(repoUrl)
+                .setDirectory(new File(destinationDir))
+                .call()) {
+            return git.getRepository().getDirectory().getAbsolutePath();
+        }
+    }
 
 }
