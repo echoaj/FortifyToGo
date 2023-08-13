b477915d80ac7e329e66e6af2dd46476f5ed4a05
echoaj <aljoslin13@yahoo.com>
2023-08-12 18:29:13 -0700

    fixed several issues

--- src/main/java/com/ng/fortifytogo/controllers/WebController.java
+++ src/main/java/com/ng/fortifytogo/controllers/WebController.java
@@ -2,30 +2,51 @@ package com.ng.fortifytogo.controllers;
 
 import com.ng.fortifytogo.services.GitService;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Controller
 public class WebController {
 
     private final GitService gitService = new GitService();
+    private String repoURL;
 
     @GetMapping(path="/" )
     public String home(){
         return "home";
     }
 
-    @PostMapping(path="/generateReport")
-    public String genReport(@RequestParam("githubLink") String githubLink, Model model) throws GitAPIException {
-        System.out.println(githubLink);
-        String localRepoPath = gitService.cloneRepository(githubLink, "C:\\Users\\aljos\\OneDrive\\Desktop\\AlexShare");
-        System.out.println(localRepoPath);
-        model.addAttribute("githubLink", githubLink);
-        return "report";
+    @PostMapping(path="/selectBranch")
+    public String selectBranch(@RequestParam("repoURL") String repositoryURL, Model model) throws GitAPIException {
+        repoURL = repositoryURL;
+        List<String> branches = gitService.getBranches(repoURL);
+        System.out.println(repoURL);
+
+        for (String b: branches){
+            System.out.println(b);
+        }
+
+        model.addAttribute("repoBranches", branches);
+        return "home";
     }
 
+    @PostMapping(path="/generateReport")
+    public String generateReport(@RequestParam String branch, Model model) throws GitAPIException {
+        System.out.println("This IS THE BRANCH YOU SELECTED!!!");
+        System.out.println(branch);
+//        String localRepoPath = gitService.cloneRepository(repoURL, "C:\\Users\\aljos\\OneDrive\\Desktop\\AlexShare");
+//        String localRepoPath = gitService.cloneRepository(repoURL, "C:\\Users\\aljos\\OneDrive\\Desktop\\AlexShare");
+//        System.out.println(localRepoPath);
+        model.addAttribute("repoURL", repoURL);
+        model.addAttribute("branch", branch);
+        return "report";
+    }
 
 }
--- src/main/java/com/ng/fortifytogo/services/GitService.java
+++ src/main/java/com/ng/fortifytogo/services/GitService.java
@@ -1,10 +1,17 @@
 package com.ng.fortifytogo.services;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class GitService {
@@ -18,4 +25,12 @@ public class GitService {
         }
     }
 
+    public List<String> getBranches(String repoUrl) throws GitAPIException {
+        List<String> branches = new ArrayList<>();
+        Collection<Ref> refs = Git.lsRemoteRepository().setHeads(true).setRemote(repoUrl).call();
+        for (Ref ref : refs) {
+            branches.add(ref.getName().replace("refs/heads/", ""));
+        }
+        return branches;
+    }
 }
